d5f1cb56e174aa62e3bedc75591b4a98badc4024
Mostafa Taman <mostafa.taman.me@gmail.com>
2023-12-20 15:15:23 +0200

    feat(auth): add middleware to protect routes & fix image hostname issue for google auth

---
+++ middleware.ts
@@ -0,0 +1,9 @@
+import { withAuth } from "@kinde-oss/kinde-auth-nextjs/middleware";
+
+export default function middleware(req: unknown) {
+    return withAuth(req);
+}
+
+export const config = {
+    matcher: ["/dashboard/:path*", "/auth-callback"],
+};
--- next.config.js
+++ next.config.js
@@ -5,6 +5,16 @@ const nextConfig = {
         config.resolve.alias.encoding = false;
         return config;
     },
+    images: {
+        remotePatterns: [
+            {
+                protocol: "https",
+                hostname: "\"lh3.googleusercontent.com\"",
+                port: "",
+
+            },
+        ],
+    }
 };
 
 module.exports = nextConfig;
